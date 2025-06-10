60dec6d788096fc0825509bb1ebe9465d4065fee
BishalJung <bishaljung63@gmail.com>
2025-06-09 23:52:11 -0400

    pagination added for scrappy so it can go all the way to available pages and scrap data

--- scrappy/scrappy/spiders/quotes_spider.py
+++ scrappy/scrappy/spiders/quotes_spider.py
@@ -18,19 +18,27 @@ class QuoteSpidr(scrapy.Spider):
         #     'titletext' : title,
         # }
         
-        items = ScrappyItem() 
-        
+         
         all_div_quotes = response.css('div.quote')
-        
+
         for quotes in all_div_quotes: ## looping through all text in the website
-            title = quotes.css('span.text::text').extract()
-            author = quotes.css("small.author::text").extract()
-            tags = quotes.css("a.tag::text").extract()
-            ## yield is used with generator and it bascially works like return
+            ## instianciate scrappyitem items.py scrappyItem class
+            items = ScrappyItem() 
             
+            title = quotes.css('span.text::text').get()
+            author = quotes.css(".author::text").get()
+            tags = quotes.css(".tag::text").extract()
+            ## yield is used with generator and it bascially works like return
             ## using inheritance to store title, author and tags name in the items class
             items['quote_title'] = title
             items['author_name'] = author
             items['quote_tag'] = tags
+            yield items ##all of the items returned properly
             
-            yield  items ##al of the items returned properly
+        
+        ## implementing pagination
+        ## attr gets attribute of the page
+        next_page = response.css('li.next a::attr(href)').get()
+        ## not None helps to prevent scrappy from scrapping empty website
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
