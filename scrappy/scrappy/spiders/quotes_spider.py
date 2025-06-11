ae5ddf09b5f95c86fc7ac6a1c4ebca14a6d83b82
BishalJung <bishaljung63@gmail.com>
2025-06-10 22:43:54 -0400

    made some changes in spider file

--- scrappy/scrappy/spiders/quotes_spider.py
+++ scrappy/scrappy/spiders/quotes_spider.py
@@ -7,14 +7,15 @@ from ..items import ScrappyItem ## inheritance of the items class properties
 class QuoteSpidr(scrapy.Spider):
     name = 'quotes' ## this is the name of our spider ## to run the spider we need this name
     ## spider crawl {name}
+    
     start_urls = [ ## we can use list to pass multiple url in the spider
         'https://quotes.toscrape.com/'
     ]
     
     def parse(self, response): ### parse method to get the response from the url
         # title = response.css('title::text').extract() ## :: tells scrapy to get text inside title
-        ## .extract() extract only the actual class data (only one)
-        # yield { ## yield is used with generator and it bascially works like return
+        ## --------  .extract() extract only the actual class data (only one) -------
+        # yield { ## -------- yield is used with generator and it bascially works like return-------- 
         #     'titletext' : title,
         # }
         
@@ -22,7 +23,7 @@ class QuoteSpidr(scrapy.Spider):
         all_div_quotes = response.css('div.quote')
 
         for quotes in all_div_quotes: ## looping through all text in the website
-            ## instianciate scrappyitem items.py scrappyItem class
+            ## instianciate scrapyitem items.py scrapyItem class
             items = ScrappyItem() 
             
             title = quotes.css('span.text::text').get()
